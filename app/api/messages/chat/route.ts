bde78fb97548c0b320f9b1f2b4e32de6b1d9185e
mahmoudmohamed <mmaahm2002@gmail.com>
2024-05-08 00:56:40 +0300

    "Remove token verification from GET request in chat route and comment out Chats component in layout"
    This commit removes the token verification from the GET request in the chat route and comments out the Chats component in the layout. The token verification

--- app/api/messages/chat/route.ts
+++ app/api/messages/chat/route.ts
@@ -4,9 +4,8 @@ import { verifyAuth } from '@/app/lib/verfiyAuth';
 
 export async function GET(req: NextRequest) {
     try {
-        const bearerToken = req.headers.get('authorization') as string
-        const token = bearerToken.split(' ')[1]
-        const user = await verifyAuth(token);
+
+        const user = await verifyAuth();
         if (!user) return NextResponse.json({ message: 'Please sign in' }, { status: 400 });
         const chats = await prisma.chat.findMany({
             where: {
