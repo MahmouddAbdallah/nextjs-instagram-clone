494a9a57a0783c75d56f12d43dc8a219c78d015c
mahmoudmohamed <mmaahm2002@gmail.com>
2024-05-08 09:14:36 +0300

    "Update messages page and remove socket setup from separate file"
    This diff shows changes to the `app/messages/page.tsx` file and the removal of the `app/socket/socket.js` file. The `page

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -1,44 +1,12 @@
-"use client";
+import React from 'react'
+import Connect from './component/Connect'
 
-import { useEffect, useState } from "react";
-import { socket } from "../socket/socket";
-
-export default function Home() {
-    const [isConnected, setIsConnected] = useState(false);
-    const [transport, setTransport] = useState("N/A");
-
-    useEffect(() => {
-        if (socket.connected) {
-            onConnect();
-        }
-
-        function onConnect() {
-            setIsConnected(true);
-            setTransport(socket.io.engine.transport.name);
-
-            socket.io.engine.on("upgrade", (transport) => {
-                setTransport(transport.name);
-            });
-        }
-
-        function onDisconnect() {
-            setIsConnected(false);
-            setTransport("N/A");
-        }
-
-        socket.on("connect", onConnect);
-        socket.on("disconnect", onDisconnect);
-
-        return () => {
-            socket.off("connect", onConnect);
-            socket.off("disconnect", onDisconnect);
-        };
-    }, []);
-
+const page = () => {
     return (
         <div>
-            <p>Status: {isConnected ? "connected" : "disconnected"}</p>
-            <p>Transport: {transport}</p>
+            <Connect />
         </div>
-    );
+    )
 }
+
+export default page
